a409b4eed1f7786bdc1fc3a0e5c92ce1eff6536d
dldnlee <sungeun8877@gmail.com>
2024-07-21 12:58:09 +0900

    [Feat] - Section Three keypoints mapped

--- app/sections/SectionThree.tsx
+++ app/sections/SectionThree.tsx
@@ -1,12 +1,33 @@
 
+const sellingPoints = [
+  {
+    heading: 'Agility and Innovation',
+    subheading: 'We keep our clients ahead with agile, innovative strategies that adapt to market changes.'
+  },
+  {
+    heading: 'Client-Centric Approach',
+    subheading: "We prioritize understanding and addressing each client's specific needs."
+  },
+  {
+    heading: 'Integrity and Transparency',
+    subheading: 'We uphold the highest ethical standards to build trust and strong partnerships.'
+  },
+]
 
-function SellingPoint() {
+interface SellingPointProps {
+  index: number,
+  heading: string,
+  subheading: string
+}
+
+
+function SellingPoint({index, heading, subheading}: SellingPointProps) {
   return (
     <div className="flex gap-4">
-      <p className="px-4 h-fit py-1 rounded-full w-fit bg-black text-white text-center">1</p>
+      <p className="px-4 h-fit py-1 rounded-full w-fit bg-black text-white text-center">{index}</p>
       <div>
-        <h3 className="text-[21px] font-bold">Easy Booking</h3>
-        <p className="text-[14px] max-w-[339px]">With lots of unique blocks, you can easily build a page without coding. </p>
+        <h3 className="text-[21px] font-bold">{heading}</h3>
+        <p className="text-[14px] max-w-[339px]">{subheading}</p>
       </div>
     </div>
   )
@@ -19,10 +40,14 @@ export default function SectionThree() {
         <h2 className="text-2xl font-extrabold">Why you should choose us?</h2>
         <p className="max-w-[600px]">Origin Consulting Group stands out for its specialized focus on startups and medium-sized businesses, combining sector-specific insights with a personalized approach to consulting.</p>
       </div>
-      <div className="w-full h-full flex gap-10 justify-center">
-        <div className="min-w-[723px] min-h-[436px] bg-gray-3"></div>
-        <div>
-          <SellingPoint /> 
+      <div className="w-full h-full flex gap-10 justify-center items-center">
+        <div className="min-w-[723px] min-h-[436px] bg-gray-3 rounded-lg"></div>
+        <div className="flex flex-col gap-14">
+          {
+            sellingPoints.map((item, index) => (
+              <SellingPoint key={index} index={index+1} heading={item.heading} subheading={item.subheading} />
+            ))
+          }
         </div>
       </div>
 

a3ee768e1b158d3a16eeb16b83a382af16ddfb25
dldnlee <sungeun8877@gmail.com>
2024-07-22 12:05:07 +0900

    [Feat] - SectionFour init

--- app/sections/SectionFour.tsx
+++ app/sections/SectionFour.tsx
@@ -1,9 +1,28 @@
 import SlidingBanner from "./components/SlidingBanner"
 
+const reviews = [
+  {
+    review: "Origin Consulting transformed our business strategy, driving impressive growth and efficiency. Highly recommended!"
+  },
+  {
+    review: "Their personalized approach and deep understanding of our needs made a significant difference. Outstanding service!"
+  },
+  {
+    review: "The team's innovative solutions and agile methods have taken our company to new heights. Exceptional consulting."
+  },
+  {
+    review: "Origin Consulting's commitment to excellence and client satisfaction is evident in every interaction. Top-notch professionals."
+  },
+  {
+    review: "Origin Consulting's sector-specific expertise and personalized approach make them stand out."
+  },
+  
+]
+
 function Reviews() {
   return (
     <div>
-
+      
     </div>
   )
 }
@@ -11,7 +30,15 @@ function Reviews() {
 export default function SectionFour() {
   return (
     <div>
-      <SlidingBanner /> 
+      <SlidingBanner>
+        {
+          reviews.map((item, index) => (
+            <li key={index}>
+              <p>{item.review}</p>
+            </li>
+          ))
+        }
+      </SlidingBanner> 
     </div>
   )
 }
--- app/sections/components/SlidingBanner.tsx
+++ app/sections/components/SlidingBanner.tsx
@@ -7,40 +7,17 @@ interface SlidingBannerProps {
 export default function SlidingBanner({children}: SlidingBannerProps) {
   return (
     <div
-                    x-data="{}"
-                    x-init="$nextTick(() => {
-                        let ul = $refs.logos;
-                        ul.insertAdjacentHTML('afterend', ul.outerHTML);
-                        ul.nextSibling.setAttribute('aria-hidden', 'true');
-                    })"
-                    className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
-                >
-                    <ul x-ref="logos" className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                        <li>
-                            <h1>Hello</h1>
-                        </li>
-                    </ul>                
-                </div>
+        x-data="{}"
+        x-init="$nextTick(() => {
+            let ul = $refs.logos;
+            ul.insertAdjacentHTML('afterend', ul.outerHTML);
+            ul.nextSibling.setAttribute('aria-hidden', 'true');
+        })"
+        className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
+    >
+        <ul x-ref="logos" className="flex gap-20 text-center items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+          {children} 
+        </ul>                
+    </div>
   )
 }

feff98797457b00270f0a03afc84e8de5974d3f1
dldnlee <sungeun8877@gmail.com>
2024-08-12 14:57:39 +0900

    [Feat] - Services Catalogue added

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,6 +3,9 @@ import SectionTwo from "./service-sections/SectionTwo"
 import SectionThree from "./service-sections/SectionThree"
 import SectionFour from "./service-sections/SectionFour"
 import SectionFive from "./service-sections/SectionFive"
+import Link from "next/link"
+import Image from "next/image"
+import rightArrow from "@/app/assets/icons/right-arrow.png";
 
 const catalogue = [
   {
@@ -10,33 +13,61 @@ const catalogue = [
     link: '/helo'
   },
   {
-    title: 'Startup Advisory',
+    title: 'Growth Strategy for Medium Enterprises',
     link: '/helo'
   },
   {
-    title: 'Startup Advisory',
+    title: 'Operational Efficiency',
     link: '/helo'
   },
   {
-    title: 'Startup Advisory',
+    title: 'Digital Transformation',
     link: '/helo'
   },
   {
-    title: 'Startup Advisory',
+    title: 'Strategy Consulting',
     link: '/helo'
   },
 ]
 
+function ServiceBlock({title, link} : {title:string, link:string}) {
+  return (
+    <Link href={link} className="border w-[500px] h-[200px] p-5 px-10 border-black/50 flex flex-col gap-5 bg-white transition-all hover:shadow-xl hover:scale-[105%] group">
+      <h3 className="text-2xl group-hover:font-semibold transition-all h-[100px] w-[400px]">{title}</h3>
+      <hr  className="border-black/20"/>
+      <p className="flex gap-5 text-lg items-center">
+        Learn More
+        <Image src={rightArrow} alt="Right Arrow" className="w-[20px] h-fit group-hover:translate-x-4 transition-all" /> 
+      </p>
+    </Link>
+  )
+}
 
+function Catalogue() {
+  return (
+    <div className="flex flex-col p-default-padding gap-10 pb-[200px]">
+      <h2 className="text-4xl font-bold">Our Services</h2>
+      <hr />
+      <div className="grid grid-cols-3 w-fit gap-14">
+        {
+          catalogue.map((item, idx) => (
+            <ServiceBlock key={idx} title={item.title} link={item.link} />
+          ))
+        }
+      </div>
+    </div>
+  )
+}
 
 export default function ServicesPage() {
   return (
-    <div>
+    <div className="bg-white">
       <SectionOne />
-      <SectionTwo />
+      <Catalogue />
+      {/* <SectionTwo />
       <SectionThree />
       <SectionFour />
-      <SectionFive />
+      <SectionFive /> */}
     </div>
   )
 }

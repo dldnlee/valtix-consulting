e751cf31a6ab102e0b982009f1406cb8016a2c2d
dldnlee <sungeun8877@gmail.com>
2024-09-10 23:05:00 +0900

    [Merge] -Merge commit

--- app/sections/SectionTwo.tsx
+++ app/sections/SectionTwo.tsx
@@ -13,7 +13,7 @@ export default function SectionTwo() {
       <div className='flex 2xl:flex-row flex-col bg-gray-4 text-gray-1 2xl:p-default-padding p-medium-padding gap-20 items-center justify-center'>
         <div className='flex flex-col justify-center 2xl:gap-10 gap-5'>
           <h2 className='font-extrabold 2xl:text-3xl text-2xl'>Journey to Success</h2>
-          <p className='max-w-[600px] 2xl:text-medium text-sm 2xl:leading-8'>{companyOverviewText}</p>
+          <p className='max-w-[600px] 2xl:text-md text-sm 2xl:leading-8'>{companyOverviewText}</p>
           <Button text={"Learn More"} padding={'p-[20px]'} bgColor='bg-beige' textColor='text-white' route='/services'/>
         </div>
         <div className='2xl:block hidden h-[500px] w-[1px] bg-black/20'></div>

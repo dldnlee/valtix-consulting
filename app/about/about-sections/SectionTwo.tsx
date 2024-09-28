daf3c9d64a4e387f07ef324f71406fa13f229e3e
dldnlee <sungeun8877@gmail.com>
2024-09-28 11:37:39 +0900

    [Fix] - Mockup banner bottom padding added

--- app/about/about-sections/SectionTwo.tsx
+++ app/about/about-sections/SectionTwo.tsx
@@ -4,7 +4,7 @@ import bannerMockup from '@/app/assets/images/billboard-mockup.webp'
 
 export default function SectionTwo() {
   return (
-    <div className="flex 2xl:flex-row flex-col items-center justify-center 2xl:p-default-padding p-medium-padding gap-20 pb-0 bg-white-abstract-pattern-3 bg-cover bg-center bg-no-repeat lg:pt-[100px] md:pt-[100px] ">
+    <div className="flex 2xl:flex-row flex-col items-center justify-center 2xl:p-default-padding p-medium-padding gap-20 2xl:pb-0 bg-white-abstract-pattern-3 bg-cover bg-center bg-no-repeat 2xl:pt-[100px] md:pt-[100px] ">
       <Image src={bannerMockup} alt="Origin Banner Mockup" className="2xl:w-[600px] w-[400px]"/>
       <div className="flex flex-col gap-7 md:text-sm lg:text-md mb-[70px]">
         <h2 className="font-bold 2xl:text-3xl text-lg">Experts from each <br></br> field worldwide.</h2>

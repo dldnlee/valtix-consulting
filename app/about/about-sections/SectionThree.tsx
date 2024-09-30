7da9d62b8be1fe5a7e032a08aea633a4f1039e35
dldnlee <sungeun8877@gmail.com>
2024-09-30 23:30:54 +0900

    [Design] - Founder card background color changed - Text edited

--- app/about/about-sections/SectionThree.tsx
+++ app/about/about-sections/SectionThree.tsx
@@ -4,7 +4,7 @@ import Image, { StaticImageData } from 'next/image'
 
 function FounderCard() {
   return (
-    <div className='flex 2xl:flex-row flex-col items-center 2xl:shadow-lg 2xl:w-[1100px]'>
+    <div className='flex 2xl:flex-row flex-col items-center 2xl:shadow-lg 2xl:w-[1100px] bg-gray-1 text-white'>
       <Image src={maykel} alt="" className='w-[400px]'/>
       <div className='flex flex-col gap-2 2xl:p-20 pt-10'>
         <div className='flex flex-col gap-1'>
@@ -17,7 +17,6 @@ function FounderCard() {
           </p>
           <p>
             With extensive experience in wealth management, management, and entrepreneurship, Maykel&apos;s goal is to provide tailored solutions for immediate and long-term success. He aims to establish Origin Consulting Group as a trusted partner known for innovation, efficiency, and excellence, helping businesses thrive in today’s competitive landscape.
-            and instead of Meet the team, replace it with Origin defined
           </p>
         </div>
       </div>
@@ -25,24 +24,11 @@ function FounderCard() {
   )
 }
 
-function MemberCard({name, position, profilePic} : {name: string, position: string, profilePic: StaticImageData}) {
-  return (
-    <div className="flex flex-col gap-10">
-      <Image src={profilePic} alt={name} className='w-[300px] h-[400px] rounded-2xl object-cover shadow-xl shadow-gray-2'/>
-      <div>
-        <h3 className="font-semibold text-lg">{name}</h3>
-        <p className="font-extralight">{position}</p>
-      </div>
-    </div>
-  )
-}
-
-
 export default function SectionThree() {
   return (
     <div className="flex flex-col items-center justify-center lg:p-default-padding p-medium-padding 2xl:gap-20 gap-10 lg:py-[120px] md:py-[120px] bg-white">
       <div className="text-center">
-        <h2 className="font-bold 2xl:text-3xl text-xl">Meet our Founder</h2>
+        <h2 className="font-bold 2xl:text-3xl text-xl">Origin Defined</h2>
         <p className="2xl:w-[600px] 2xl:text-md text-sm">The driving force behind innovative solutions and strategic growth. Our consultants are here to empower your business and deliver exceptional results.</p>
       </div>
       <FounderCard />

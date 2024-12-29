d86d48bb7e8889e759a125daca1b03569369f362
dldnlee <sungeun8877@gmail.com>
2024-12-29 15:51:20 +0900

    [Feat] - Changed SectionThree founder card layout

--- app/about/about-sections/SectionThree.tsx
+++ app/about/about-sections/SectionThree.tsx
@@ -1,25 +1,32 @@
 import maykel from '@/app/assets/images/Maykel.jpg'
 import Image, { StaticImageData } from 'next/image'
-
+// import professionalImage from '@/app/assets/images/professional_image.webp';
+import paintImage from '@/app/assets/images/grayscale-paint-image.webp';
 
 function FounderCard() {
   return (
-    <div className='flex 2xl:flex-row flex-col items-center 2xl:shadow-lg 2xl:w-[1100px] bg-gray-1 text-white 2xl-rounded-none rounded-xl'>
-      <Image src={maykel} alt="" className='w-[400px] 2xl-rounded-none rounded-t-xl'/>
-      <div className='flex flex-col gap-2 2xl:p-20 pt-10 p-5'>
-        <div className='flex flex-col gap-1'>
-          <h2 className='2xl:text-xl text-lg font-extrabold'>Maykel Soeharto</h2>
-          <h3 className='2xl:text-lg text-md font-bold'>Managing Director & Senior Partner</h3>
-        </div>
-        <div className='flex flex-col gap-5 max-w-[600px]'>
-          <p>
-            Maykel Soeharto founded Valtix Consulting with a clear mission to empower start-ups and medium-sized enterprises through innovative strategies and advanced technological solutions that drive growth and streamline operations. Recognizing a gap in the consulting landscape, he sought to address the unique challenges and opportunities faced by these businesses.
+    <div className='flex w-full items-center justify-center 2xl:h-[900px]'>
+      <div className='flex 2xl:flex-col flex-col gap-5 h-full justify-center items-center 2xl:shadow-lg 2xl:w-[800px] bg-gray-1 text-white 2xl:rounded-none rounded-xl 2xl:px-20 2xl:py-16 px-5 py-4'>
+          <h2 className='2xl:text-2xl text-lg font-bold'>
+            &quot;Empowering Businesses to Thrive with Tailored Strategies and Sustainable Growth.&quot;
+          </h2>
+          <p className='2xl:text-lg text-md'>
+            At Valtix Consulting, we redefine what it means to grow and scale in today&apos;s fast-changing markets. We don&apos;t just provide advice—we craft bespoke strategies that empower startups and medium-sized enterprises to unlock their full potential.
           </p>
-          <p>
-            With extensive experience in wealth management, management, and entrepreneurship, Maykel&apos;s goal is to provide tailored solutions for immediate and long-term success. He aims to establish Valtix Consulting as a trusted partner known for innovation, efficiency, and excellence, helping businesses thrive in today’s competitive landscape.
+          <p className='2xl:text-lg text-md'>
+            Our expertise spans startup advisory, growth strategy, operational efficiency, digital transformation, and strategic planning, but what sets us apart is our relentless focus on innovation and adaptability. From creating hyper-personalized growth roadmaps to spearheading transformational change, we work as true partners, embedding ourselves in your journey to success.
           </p>
-        </div>
+          <p className='2xl:text-lg text-md'>
+            We thrive on complexity, finding clarity in chaos, and turning challenges into opportunities. At Valtix Consulting, we&apos;re not just solving today&apos;s problems; we&apos;re building tomorrow&apos;s possibilities. Let&apos;s create something extraordinary together.
+          </p>
       </div>
+
+        <Image
+        src={paintImage}
+        alt="Grayscale paint splatter image"
+        className='w-[500px] h-full object-cover object-center 2xl:block hidden'
+        />
+
     </div>
   )
 }
@@ -28,8 +35,8 @@ export default function SectionThree() {
   return (
     <div className="flex flex-col items-center justify-center p-medium-padding 2xl:gap-20 gap-10 lg:py-[120px] md:py-[120px] bg-white">
       <div className="text-center">
-        <h2 className="font-bold 2xl:text-3xl text-xl">Valtix Defined</h2>
-        <p className="2xl:w-[600px] 2xl:text-md text-sm">The driving force behind innovative solutions and strategic growth. Our consultants are here to empower your business and deliver exceptional results.</p>
+        <h2 className="font-bold 2xl:text-4xl text-2xl py-5 2xl:py-2">Valtix Defined.</h2>
+        <p className="2xl:w-[900px] 2xl:text-lg text-md">&quot;Valtix Consulting stands at the forefront of strategic business solutions in Indonesia. We empower organizations to navigate challenges, seize opportunities, and achieve sustainable growth. With a blend of local expertise and global insights, we redefine success for our clients.&quot;</p>
       </div>
       <FounderCard />
     </div>
